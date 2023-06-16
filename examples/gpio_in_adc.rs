0fde5b6b2d0ba76f318c9dca342060fa9145ed43
Darian Harrison <darianharrison89@gmail.com>
2023-06-16 00:29:49 -0500

    Update gpio_in_adc.rs

--- examples/gpio_in_adc.rs
+++ examples/gpio_in_adc.rs
@@ -1,11 +1,11 @@
-//! # Pico GPIO In/Out Example
+//! # Pico USB Serial Example
 //!
-//! Toggles the LED based on GPIO input.
+//! Creates a USB Serial device on a Pico board, with the USB driver running in
+//! the main thread.
 //!
-//! This will control an LED on GP25 based on a button hooked up to GP15. The
-//! button should cause the line to be grounded, as the input pin is pulled high
-//! internally by this example. When the button is pressed, the LED will turn
-//! off.
+//! This will create a USB Serial device echoing anything it receives. Incoming
+//! ASCII characters are converted to upercase, so you can tell it is working
+//! and not just local-echo!
 //!
 //! See the `Cargo.toml` file for Copyright and license details.
 
@@ -14,9 +14,6 @@
 
 // The macro for our start-up function
 use rp_pico::entry;
-
-// GPIO traits
-use embedded_hal::digital::v2::{InputPin, OutputPin};
 
 // Ensure we halt the program on panic (if we don't mention this crate it won't
 // be linked)
@@ -30,22 +27,60 @@ use rp_pico::hal::pac;
 // higher-level drivers.
 use rp_pico::hal;
 
+
+// Some traits we need
+use embedded_hal::adc::OneShot;
+
+
+// Pull in any important traits
+use rp_pico::hal::prelude::*;
+
+// USB Device support
+use usb_device::{class_prelude::*, prelude::*};
+
+// USB Communications Class Device support
+use usbd_serial::SerialPort;
+
+// Used to demonstrate writing formatted strings
+use core::fmt::Write;
+use heapless::String;
+
 /// Entry point to our bare-metal application.
 ///
 /// The `#[entry]` macro ensures the Cortex-M start-up code calls this function
 /// as soon as all global variables are initialised.
 ///
-/// The function configures the RP2040 peripherals, then just reads the button
-/// and sets the LED appropriately.
+/// The function configures the RP2040 peripherals, then echoes any characters
+/// received over USB Serial.
 #[entry]
 fn main() -> ! {
     // Grab our singleton objects
     let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
 
-    // Note - we don't do any clock set-up in this example. The RP2040 will run
-    // at it's default clock speed.
+    // Set up the watchdog driver - needed by the clock setup code
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
 
-    // The single-cycle I/O block controls our GPIO pins
+    // Configure the clocks
+    //
+    // The default is to generate a 125 MHz system clock
+    let clocks = hal::clocks::init_clocks_and_plls(
+        rp_pico::XOSC_CRYSTAL_FREQ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    // The delay object lets us wait for specified amounts of time (in
+    // milliseconds)
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+
+    // The single-cycle I/O block controls our GPIO 
     let sio = hal::Sio::new(pac.SIO);
 
     // Set the pins up according to their function on this particular board
@@ -56,19 +91,63 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // Our LED output
-    let mut led_pin = pins.led.into_push_pull_output();
+    // Set up the USB driver
+    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
+        pac.USBCTRL_REGS,
+        pac.USBCTRL_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut pac.RESETS,
+    ));
 
-    // Our button input
-    let button_pin = pins.gpio15.into_pull_up_input();
+    // Set up the USB Communications Class Device driver
+    let mut serial = SerialPort::new(&usb_bus);
 
-    // Run forever, setting the LED according to the button
+    // Create a USB device with a fake VID and PID
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+        .manufacturer("Fake company")
+        .product("Serial port")
+        .serial_number("TEST")
+        .device_class(2) // from: https://www.usb.org/defined-class-codes
+        .build();
+
+    let timer = hal::Timer::new(pac.TIMER, &mut pac.RESETS);
+    let mut counter: u32 = 0;
+
+
+    // Enable ADC
+    let mut adc = hal::Adc::new(pac.ADC, &mut pac.RESETS);
+
+    // Enable the temperature sense channel
+    let mut temperature_sensor = adc.enable_temp_sensor();
+
+    // Configure GPIO28 as an ADC input
+    let mut adc_pin_0 = pins.gpio28.into_floating_input();
+
     loop {
-        if button_pin.is_low().unwrap() {
-            led_pin.set_high().unwrap();
-        } else {
-            led_pin.set_low().unwrap();
+
+        // poll usb every 10 ms
+        usb_dev.poll(&mut [&mut serial]);
+
+        // nota: 1_000_000 microsegundos = 1 segundo
+        if counter % 1_000_000 == 0 { // cada segundo
+
+            // Read the raw ADC counts from the temperature sensor channel.
+            let temp_sens_adc_counts: u16 = adc.read(&mut temperature_sensor).unwrap();
+            let pin_adc_counts: u16 = adc.read(&mut adc_pin_0).unwrap();
+
+            // convertir a texto solo para efectos de imprimir a consola (en produccion puedes mandar el puro binario)
+            let mut text: String<32> = String::new();
+            writeln!(&mut text, "\n\rCurrent counter: {}\r\n", pin_adc_counts).unwrap();
+
+            // This only works reliably because the number of bytes written to
+            // the serial port is smaller than the buffers available to the USB
+            // peripheral. In general, the return value should be handled, so that
+            // bytes not transferred yet don't get lost.
+            let _ = serial.write(text.as_bytes());
+
         }
+        counter = counter + 1
     }
 }
 
