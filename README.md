851c877e651e9e19dec12425866df6ae2a44eacf
Darian Harrison <darianharrison89@gmail.com>
2023-06-20 07:18:14 -0500

    a

--- README.md
+++ README.md
@@ -13,10 +13,10 @@ https://pdf1.alldatasheet.com/datasheet-pdf/view/47503/SONY/ILX526A.html
 
 
 
-
 ### GPIO ADC to USB HOST
 
 1.) Wires
+
 
 ![Alt Text](./docs/photoresistor.png)
 
@@ -26,25 +26,25 @@ https://pdf1.alldatasheet.com/datasheet-pdf/view/47503/SONY/ILX526A.html
 cargo run --release --example gpio_in_adc
 ```
 
-
 3.) run core to read data over usb
 https://github.com/DarianHarrison/l10_core
 ```sh
 cargo run 0x16c0 0x27dd
 ```
 
+
 ## sources
 
-// board-crates
-https://crates.io/crates/embedded-hal
-https://crates.io/crates/rp2040-hal
-https://crates.io/crates/rp-pico
+ // board-crates
+ https://crates.io/crates/embedded-hal
+ https://crates.io/crates/rp2040-hal
+ https://crates.io/crates/rp-pico
 
-// arm-crates
-https://crates.io/crates/cortex-m
+ // arm-crates
+ https://crates.io/crates/cortex-m
 
-// core
-usb host system
-https://github.com/rust-embedded-community/usbd-serial
-https://github.com/a1ien/rusb
-https://github.com/rust-embedded-community/usb-device
+ // core
+ usb host system
+ https://github.com/rust-embedded-community/usbd-serial
+ https://github.com/a1ien/rusb
+ https://github.com/rust-embedded-community/usb-device
--- examples/adc_to_usb.rs
+++ examples/adc_to_usb.rs
@@ -27,10 +27,8 @@ use rp_pico::hal::pac;
 // higher-level drivers.
 use rp_pico::hal;
 
-
 // Some traits we need
 use embedded_hal::adc::OneShot;
-
 
 // Pull in any important traits
 use rp_pico::hal::prelude::*;
@@ -135,7 +133,7 @@ fn main() -> ! {
 
         // convertir a texto solo para efectos de imprimir a consola (en produccion puedes mandar el puro binario)
         let mut text: String<32> = String::new();
-        writeln!(&mut text, "\n\rCurrent counter: {}\r\n", pin_adc_counts).unwrap();
+        writeln!(&mut text, "Current counter: {pin_adc_counts}").unwrap();
 
         // This only works reliably because the number of bytes written to
         // the serial port is smaller than the buffers available to the USB
@@ -146,3 +144,33 @@ fn main() -> ! {
 }
 
 // End of file
+/*let mut serial = SerialPort::new(&usb_bus);
+
+let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+    .product("Serial port")
+    .device_class(USB_CLASS_CDC)
+    .build();
+
+loop {
+    if !usb_dev.poll(&mut [&mut serial]) {
+        continue;
+    }
+
+    let mut buf = [0u8; 64];
+
+    match serial.read(&mut buf[..]) {
+        Ok(count) => {
+            // count bytes were read to &buf[..count]
+        },
+        Err(UsbError::WouldBlock) => // No data received
+        Err(err) => // An error occurred
+    };
+
+    match serial.write(&[0x3a, 0x29]) {
+        Ok(count) => {
+            // count bytes were written
+        },
+        Err(UsbError::WouldBlock) => // No data could be written (buffers full)
+        Err(err) => // An error occurred
+    };
+}*/
